6bc2448e3bcd651b7573d57396bb1f3aea9ae32f
davmrazek <davmrazek@seznam.cz>
2025-12-03 20:47:30 +0100

    Task 3 Part2 - working only on easy strings

--- task3.cpp
+++ task3.cpp
@@ -5,25 +5,43 @@
 
 int main(){
     std::string row;
-    int sum = 0;
-
+    long long sum = 0;
     // looping through all the rows
     while (std::cin >> row)
     {
-        char first = '0', second = '0';
+        char nums [13];
+        // filling with 0s
+        for(int i = 0; i < 13; i++){
+            nums[i] = '0';
+        }
         int length = row.size();
+
         // looping through each character
         for(int i = 0; i < length; i++) {
             char current = row[i];
-            if (current > first && (i + 1 < length)) {
-                first = current;
-                second = 0;
-            }
-            else if (current > second){
-                second = current;
+            int next = 1;
+            int index = 0;
+            while (next && index < 13)
+            {
+                // rewriting num
+                // i = 7, current = 1
+                if (nums[index] < current && (length - i >= 13 - index))
+                {
+                    nums[index] = current;
+                    next = 0;
+                }
+                else {
+                    index++;
+                }
             }
         }
-        sum += ((first - '0') * 10 + (second - '0'));
+        
+        // adding to sum
+        long long multiplier = 100000000000;
+        for(char num : nums){
+            sum+= (num - '0') * multiplier;
+            multiplier /= 10;
+        }
     }
 
     std::cout << sum << std::endl;
